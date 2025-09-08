dfddd265fcc8b2cf33184380b0a4766733373e42
Tyler Huyser <tyler.huyser@gmail.com>
2025-09-07 23:54:52 -0400

    created boilerplat Mission.css

---
+++ client/app/_components/_sections/_mission/Mission.tsx
@@ -0,0 +1,16 @@
+import './Mission.css'
+
+export default function Mission() {
+  return (
+    <div className="section" id="mission-section">
+      <div className="mission-subsection subsection" id="mission-copy">
+        AWOKEN strengthens teams and communities through immersive technology and human-centered storytelling.
+
+        We build experiences that confront bias, shift perspectives, and create spaces where everoyone feels seen, heard, and valued.
+      </div>
+      <div className="mission-subsection subsection" id="mission-image">
+        <img src="/images/login-carousel-gif-2.gif" />
+      </div>  
+    </div>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Watch from "./_components/_sections/_watch/Watch"
+import Mission from "./_components/_sections/_mission/Mission"
 
 export default function Home() {
   return (
     <>
       <Watch />
+      <Mission />
     </>
   );
 }
