e62e4e1941771aa819e43051168ae3bff9eda5e8
Tyler Huyser <tyler.huyser@gmail.com>
2025-09-12 16:36:15 -0400

    added navy blue background to components - also changed the way fonts scale in mission and platfomr components using viewport width measurments. Lastly, added relative and absolute positioning to the Carousel of the app renders in the Platform Component to stick it at the bottom of the component.

--- client/app/_components/_sections/_mission/Mission.tsx
+++ client/app/_components/_sections/_mission/Mission.tsx
@@ -7,12 +7,13 @@ export default function Mission() {
   return (
     <section className="section" id={styles.missionSection}>
 
-      <div className={`subsection ${styles.missionSubsection}`} id={styles.missionTextSubsection}>
-        <p className={styles.missionText} id={styles.missionTitle}><strong className={styles.boldText}>AWOKEN</strong> strengthens teams and communities through immersive technology and human-centered storytelling.</p>
-        <p className={styles.missionText} id={styles.missionSubtitle}>We build experiences that confront bias, shift perspectives, and create spaces where everoyone feels seen, heard, and valued.</p>
-      </div>
-      
       <Carousel carouselType="missionCarousel" data={gifs} />
+
+      <div className={`subsection ${styles.missionSubsection}`} id={styles.missionTextSubsection}>
+        <p className={styles.missionText} id={styles.missionTitle}><strong className={styles.boldText}>AWOKEN</strong> strengthens teams and communities.</p>
+        <p className={styles.missionText} id={styles.missionSubtitle}>Using immersive technology and human-centered storytelling, we build experiences that confront bias, shift perspectives, and create spaces where everoyone feels seen, heard, and valued.</p>
+      </div>
+    
     </section>
   );
 }
