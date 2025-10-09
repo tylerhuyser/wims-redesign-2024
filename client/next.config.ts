8a46c6a63022d6ffc9c40878a4b0209388602067
Tyler Huyser <tyler.huyser@gmail.com>
2025-10-09 13:13:40 -0400

    updated next.config to allow for React-Panelluim to work.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    transpilePackages: ["react-pannellum-next"],
+  },
 };
 
 export default nextConfig;
