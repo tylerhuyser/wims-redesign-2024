955bbcfdab80a417816e82fef3e0bdda5dc6e53c
Tyler Huyser <tyler.huyser@gmail.com>
2025-10-09 13:17:00 -0400

    mopved transpile packages out of experimental within next.config for deployment.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore
-    transpilePackages: ["react-pannellum-next"],
-  } as any,
+  transpilePackages: ["react-pannellum-next"]
 };
 
 export default nextConfig;
