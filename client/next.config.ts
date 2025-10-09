b8b02e61b80bddb981e695385980a1b6b65f0da5
Tyler Huyser <tyler.huyser@gmail.com>
2025-10-09 13:14:53 -0400

    resolved type error in next.config.

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    // @ts-ignore
     transpilePackages: ["react-pannellum-next"],
-  },
+  } as any,
 };
 
 export default nextConfig;
