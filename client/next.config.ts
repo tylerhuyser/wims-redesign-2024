75b57650ce21049fce2f380ccd82eb8ed6eaad8f
Tyler Huyser <tyler.huyser@gmail.com>
2025-10-27 20:05:10 -0400

    updated netlify config, build command, and export settings.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["react-pannellum-next"]
+  transpilePackages: ["react-pannellum-next"],
+  output: "export"
 };
 
 export default nextConfig;
